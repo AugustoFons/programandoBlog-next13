faf4b9c38af480aad3a94704370fe2ce6e446f19
AugustoFons <111530986+AugustoFons@users.noreply.github.com>
2023-07-05 16:11:51 -0300

    footer center

---
+++ app/feed/page.jsx
@@ -0,0 +1,83 @@
+'use client'
+
+import PublicationCard from "@components/PublicationCard"
+import { useState, useEffect } from "react"
+import SpinnerFeed from "@components/SpinnerFeed"
+
+const PublicationCardList = ({ data, handleTagClick }) => {
+    return (
+        <div className="mt-16 publication_layout">
+            {data.map((post) => (
+                <PublicationCard
+                    key={post._id}
+                    post={post}
+                    handleTagClick={handleTagClick}
+                    />
+            ))}
+        </div>
+    )
+}
+
+const Feed = () => {
+    const [allPosts, setAllPosts] = useState([])
+    const [loading, setLoading] = useState(false)
+    //buscador
+    const [searchText, setSearchText] = useState("");
+    const [searchedResults, setSearchedResults] = useState([]);
+
+    useEffect(() => {
+        setLoading(true);
+        fetch('/api/publicaciones', {
+            cache: 'no-store'
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            setAllPosts(data);
+            setLoading(false);
+            setSearchedResults(data)
+            console.log(data)
+        });
+    }, []);
+    
+
+    useEffect(() => {
+        const filteredPosts = allPosts.filter(
+        (p) =>
+            p.creator.username.toLowerCase().includes(searchText) ||
+            p.tag.toLowerCase().includes(searchText) ||
+            p.publication.toLowerCase().includes(searchText) ||
+            p.title.toLowerCase().includes(searchText)
+        );
+        setSearchedResults(filteredPosts);
+    }, [searchText]);
+
+    const handleSearchChange = (e) => {
+        setSearchText(e.target.value.toLowerCase());
+        };
+    
+    const handleTagClick = (tagName) => {
+        setSearchText(tagName.toLowerCase());
+    };
+
+    return (
+        <section className="feed">
+            <div className="relative w-full flex justify-center items-center">
+                <input
+                    type="text"
+                    placeholder="Busca por nombre de usuario, por contenido o tags ..."
+                    value={searchText}
+                    onChange={handleSearchChange}
+                    required
+                    className="search_input peer"
+                    />
+            </div>
+            {loading && <SpinnerFeed />}
+                <PublicationCardList
+                    data={searchedResults}
+                    handleTagClick={handleTagClick}
+                    />
+        </section>
+    )
+}
+
+export default Feed
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,18 +7,14 @@ export default function FooterPage() {
   return (
     <Footer className='w-full bg-transparent h-fit outline-0 p-4'>
       <div className="w-full border-transparent flex-col text-end">
-        <div className="w-full sm:flex sm:items-center sm:justify-between flex-col gap-3">
+        <div className="w-full sm:flex sm:items-center sm:justify-center flex-col gap-3">
         <Footer.Copyright
             by="augustofonsdev@gmail.com"
             href="#"
             year={2023}
-            className='text-end'
+            className='text-center'
           />
-          <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
-            <Footer.Icon
-              href="#"
-              icon={BsInstagram}
-            />
+          <div className="mt-4 flex space-x-6 sm:mt-0 justify-center">
             <Footer.Icon
               href="#"
               icon={BsLinkedin}
