0d830ab1bf49ec65b1b9b0949e52e33594a7c519
AugustoFons <111530986+AugustoFons@users.noreply.github.com>
2023-07-05 14:20:00 -0300

    redes footer - pasar el feed al directorio app

--- app/api/publicaciones/route.js
+++ app/api/publicaciones/route.js
@@ -1,17 +1,14 @@
 import { connectToDB } from "@utils/database";
 import PublicationUser from "@models/publication";
-import { NextRequest, NextResponse } from "next/server";
-import { revalidatePath } from "next/cache";
 
 export const GET = async (request) => {
     try {
         await connectToDB();
+        
         const publications = await PublicationUser.find({}).populate("creator");
-
-        const path = request.nextUrl.searchParams.get("path") || "/";
-        revalidatePath(path);
-        return NextResponse.json(publications);
-
+        return new Response(JSON.stringify(publications), { 
+            status: 200,
+        })
     } catch (error) {
         return new Response('Failed to fetch all prompts', {
             status: 500 
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,16 +1,36 @@
 'use client'
 
-import { Footer } from "flowbite-react"
+import { Footer } from 'flowbite-react';
+import { BsDribble, BsFacebook, BsGithub, BsInstagram, BsTwitter, BsLinkedin } from 'react-icons/bs';
 
 export default function FooterPage() {
   return (
-    <Footer container className="w-full bg-transparent border-0 flex">
-        <Footer.Copyright 
+    <Footer className='w-full bg-transparent h-fit outline-0 p-4'>
+      <div className="w-full border-transparent flex-col text-end">
+        <div className="w-full sm:flex sm:items-center sm:justify-between flex-col gap-3">
+        <Footer.Copyright
             by="augustofonsdev@gmail.com"
             href="#"
             year={2023}
-            className="w-full bg-transparent border-0 flex items-center justify-center text-center"
-        />
+            className='text-end'
+          />
+          <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
+            <Footer.Icon
+              href="#"
+              icon={BsInstagram}
+            />
+            <Footer.Icon
+              href="#"
+              icon={BsLinkedin}
+            />
+            <Footer.Icon
+              href="#"
+              icon={BsGithub}
+            />
+
+          </div>
+        </div>
+      </div>
     </Footer>
   )
 }
