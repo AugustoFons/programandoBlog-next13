28c0b86060ae8a397c40eef7f4fecd72039459ba
AugustoFons <111530986+AugustoFons@users.noreply.github.com>
2023-07-06 11:17:02 -0300

    cambios

--- app/feed/Feed.jsx
+++ app/feed/Feed.jsx
@@ -1,40 +1,10 @@
-'use client'
 
-import { useState, useEffect } from "react"
 import SpinnerFeed from "@components/SpinnerFeed"
 import PublicationCardList from "@components/PublicationCardList"
 import Search from "@components/Search"
-import { useRouter } from "next/navigation"
 
-const Feed = () => {
-    const router = useRouter()
-    const [allPosts, setAllPosts] = useState([])
-    const [loading, setLoading] = useState(false)
-    //buscador estados
-    const [searchText, setSearchText] = useState("");
-    const [searchedResults, setSearchedResults] = useState([]);
+const Feed = ({allPosts, loading, searchText, setSearchText, setSearchedResults, searchedResults}) => {
 
-
-    useEffect(() => {
-        const fetchPost = async () => {
-            try {
-                router.push('/feed')
-                setLoading(true);
-                const response = await fetch('/api/publicaciones', {
-                    cache: "no-cache",
-                    next: { revalidate: 60 },
-                }); 
-                const data = await response.json();
-    
-                setAllPosts(data);
-                setSearchedResults(data)
-                setLoading(false);
-            } catch (error) {
-                console.log(error)
-            }
-        }
-        fetchPost()
-        }, [])
 
     const handleTagClick = (tagName) => {
         setSearchText(tagName.toLowerCase());
--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,47 @@
-import Feed from "./feed/page"
+'use client'
+
+import { useState, useEffect } from "react"
+import Feed from "./feed/Feed"
 
 const Home = () => {
+    const [allPosts, setAllPosts] = useState([])
+    const [loading, setLoading] = useState(false)
+    
+    //estados para el buscador
+    const [searchText, setSearchText] = useState("");
+    const [searchedResults, setSearchedResults] = useState([]);
+
+    useEffect(() => {
+        const fetchPost = async () => {
+            try {
+                setLoading(true);
+                const response = await fetch('/api/publicaciones', {
+                    cache: "no-store",
+                }); 
+                const data = await response.json();
+    
+                setAllPosts(data);
+                setSearchedResults(data)
+                setLoading(false);
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchPost()
+        }, [])
     return (
         <section>
             
-            <Feed />
+            <Feed
+                allPosts={allPosts}
+                setAllPosts={setAllPosts}
+                loading={loading}
+                setLoading={setLoading}
+                searchText={searchText}
+                setSearchText={setSearchText}
+                setSearchedResults={setSearchedResults}
+                searchedResults={searchedResults}   
+                />
         </section>
     )
 }
