b1d1225c293a629411aa61cd293c6e390a3d31d6
AugustoFons <augustofonsdev@gmail.com>
2023-07-06 15:55:39 -0300

    correcion falla en el buscador

--- components/Search.jsx
+++ components/Search.jsx
@@ -5,7 +5,7 @@ const Search = ({searchText, setSearchText, setSearchedResults, allPosts}) => {
     useEffect(() => {
         const filteredPosts = allPosts.filter(
         (p) =>
-            p.creator.username.toLowerCase().includes(searchText) ||
+            p.username.toLowerCase().includes(searchText) ||
             p.tag.toLowerCase().includes(searchText) ||
             p.publication.toLowerCase().includes(searchText) ||
             p.title.toLowerCase().includes(searchText)
